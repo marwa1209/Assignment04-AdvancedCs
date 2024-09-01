011d9bac42b3b9a626ac16e552a245ef5d9ad948
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-01 17:32:44 +0300

    What Is Event

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             #region What Is Event
+            //event is a way for a class to notify other classes (subscribers) when something happens. It uses a delegate to define the method signature for the event handlers.
+            //The publisher class raises the event, and subscribers handle it with their methods. Events are commonly used in the publisher-subscriber model for communication between
+            //objects.
+            #endregion
+            #region Event [example 01]
 
             #endregion
         }
