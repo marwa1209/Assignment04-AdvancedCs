b665495dafc07fc6dee0598505a092afeed2b308
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-02 16:27:54 +0300

    Hashtables Category [non-generic]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.Example02_Youtube;
 using Demo.Fifa_Game;
+using System.Collections;
 
 namespace Demo
 {
@@ -36,17 +37,32 @@ namespace Demo
 
             #endregion
             #region Event [example 02]
-            Subscriber subscriber01 = new Subscriber() { Name = "Marwa" };
-            Subscriber subscriber02 = new Subscriber() { Name = "Marwan" };
-            Subscriber subscriber03 = new Subscriber() { Name = "Mary" };
-            Subscriber subscriber04 = new Subscriber() { Name = "rania" };
-            Channel channel01 = new Channel() { ChannelName = "comedy" };
-            channel01.VideoAdded += subscriber01.Notify;
-            channel01.VideoAdded += subscriber02.Notify;
-            channel01.VideoAdded += subscriber03.Notify;
-            channel01.VideoAdded += subscriber04.Notify;
-            channel01.AddVideo(new Video() { Title = "Video01", Desc = "loremmmmmmmmm" });
-            channel01.AddVideo(new Video() { Title = "Video02", Desc = "loremmmmmmmmm" });
+            //Subscriber subscriber01 = new Subscriber() { Name = "Marwa" };
+            //Subscriber subscriber02 = new Subscriber() { Name = "Marwan" };
+            //Subscriber subscriber03 = new Subscriber() { Name = "Mary" };
+            //Subscriber subscriber04 = new Subscriber() { Name = "rania" };
+            //Channel channel01 = new Channel() { ChannelName = "comedy" };
+            //channel01.VideoAdded += subscriber01.Notify;
+            //channel01.VideoAdded += subscriber02.Notify;
+            //channel01.VideoAdded += subscriber03.Notify;
+            //channel01.VideoAdded += subscriber04.Notify;
+            //channel01.AddVideo(new Video() { Title = "Video01", Desc = "loremmmmmmmmm" });
+            //channel01.AddVideo(new Video() { Title = "Video02", Desc = "loremmmmmmmmm" });
+            #endregion
+            #region  Hashtables Category[non-generic]
+            //generic
+            //non-generic
+            Hashtable Note = new Hashtable();
+            Console.WriteLine(Note.Count);
+            Note.Add("Ahmed", "012238475956");
+            Note.Add("rana", "012238475956");
+            Note.Add("marwa", "012238475956");
+            Note.Add("marwan", "012238475956");
+            Console.WriteLine(Note.Count);
+            foreach (DictionaryEntry Item in Note) {
+                Console.WriteLine(Item);
+                Console.WriteLine(Item);
+            }
             #endregion
         }
     }
